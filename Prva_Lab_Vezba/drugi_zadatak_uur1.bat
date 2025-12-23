e5c3f6bee03e2c752acaedcd1fa93aa0ef16749d
MatejaRadosavljevic <145156235+MatejaRadosavljevic@users.noreply.github.com>
2025-12-23 23:13:54 +0100

    mala ispravka u preciznosti

--- Prva_Lab_Vezba/drugi_zadatak_uur1.bat
+++ Prva_Lab_Vezba/drugi_zadatak_uur1.bat
@@ -4,10 +4,11 @@
 set /p folder=Naziv foldera: 
 set /p fajl=Naziv fajla: 
 
-cd %folder%
+cd /d %folder%
 dir > %fajl%
-copy %fajl% ..\c\k\%fajl%
 
-attib +r ..\c\k\%fajl%
-cd ..\c\k
+copy %fajl% ..\c\k\%fajl%
+attrib +r ..\c\k\%fajl%
+
+cd /d ..\c\k
 start notepad.exe %fajl%
